bbabfae7628e17cc357e17f49ea1c2de7b953dab
ferehibrahimli123@outlook.com <ferehibrahimli123@outlook.com>
2022-04-13 21:55:15 +0400

    registercommit

---
+++ Pustok/Startup.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pustok.DAL;
+using Pustok.Models;
+using Pustok.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
+
+            services.AddSession(opt => 
+            {
+                opt.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+             {
+                 options.Password.RequireDigit = true;
+                 options.Password.RequiredLength = 6;
+                 options.Password.RequireLowercase = true;
+                 options.Password.RequireUppercase = true;
+                 options.Password.RequiredUniqueChars = 1;
+
+                 options.User.RequireUniqueEmail = true;
+                 
+                 options.Lockout.AllowedForNewUsers = true;
+                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+                 options.Lockout.MaxFailedAccessAttempts = 3;
+             }).AddDefaultTokenProviders().AddEntityFrameworkStores<AppDbContext>();
+
+
+            services.AddScoped<LayoutService>();
+
+            services.AddHttpContextAccessor();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseSession();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                        name: "areas",
+                        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+
+                endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+        }
+    }
+}
---
+++ Pustok/ViewModels/Account/RegisterVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pustok.ViewModels.Account
+{
+    public class RegisterVM
+    {
+        [Required]
+        [StringLength(maximumLength:30)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30)]
+        public string SurName { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
